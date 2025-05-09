974c91d5e33fa95c251f5ee143dd1043b93c676c
Sujendran <sujendranjanagiraman@gmail.com>
2025-05-09 12:14:18 +0530

    origin update

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://talk-a-tive-3yu8.onrender.com",
     // credentials: true,
   },
 });
